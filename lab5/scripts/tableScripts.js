3cbfc928224b3b6edbfdcfad3128a016b6e7879b
max <m.d.mihailik@gmail.com>
2024-10-02 15:07:05 +0300

    feat: now cards are saved to local storage

--- lab5/scripts/tableScripts.js
+++ lab5/scripts/tableScripts.js
@@ -8,6 +8,8 @@ window.addEventListener("submit", (e) => {
 
     createNewFilmCard(filmName, filmLanguage, filmBookDuration);
 
+    saveCardsToLocalStorage(filmName, filmLanguage, filmBookDuration);
+
     form.reset();
 });
 
@@ -38,6 +40,7 @@ const createNewFilmCard = (name, language, duration) => {
     removeButton.textContent = "Remove";
     removeButton.addEventListener("click", () => {
         filmOrderCardDiv.remove();
+        removeCardFromLocalStorage(name);
     });
 
     filmOrderCardDiv.appendChild(filmOrderCardFilmName);
@@ -47,3 +50,26 @@ const createNewFilmCard = (name, language, duration) => {
     const filmCardsContainer = document.querySelector(".innerRightContainer");
     filmCardsContainer.appendChild(filmOrderCardDiv);
 };
+
+const saveCardsToLocalStorage = (name, language, duration) => {
+    let savedCards = JSON.parse(localStorage.getItem("filmCards"));
+    savedCards.push({ name, language, duration });
+    localStorage.setItem("filmCards", JSON.stringify(savedCards));
+};
+
+const removeCardFromLocalStorage = (name) => {
+    let savedCards = JSON.parse(localStorage.getItem("filmCards"));
+    savedCards = savedCards.filter((card) => card.name !== name);
+    localStorage.setItem("filmCards", JSON.stringify(savedCards));
+};
+
+const loadSavedFilmCards = () => {
+    const savedCards = JSON.parse(localStorage.getItem("filmCards"));
+    savedCards.forEach((card) => {
+        createNewFilmCard(card.name, card.language, card.duration);
+    });
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+    loadSavedFilmCards();
+});
